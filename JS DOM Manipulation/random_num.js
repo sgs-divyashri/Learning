7bc429e0dd40837f728e313cba98502464ab06a5
sgs-divyashri <divyashri.b@sgstechsolutions.com>
2025-11-08 19:22:23 +0530

    added files

---
+++ JS DOM Manipulation/random_num.js
@@ -0,0 +1,20 @@
+
+
+//Random Numbers
+var a = Math.random()
+console.log(a)
+
+var b = Math.random()
+console.log(b*10)
+
+//Floor Method
+var c = Math.floor(0.99)
+console.log(c)
+
+var d = Math.floor(56.99)
+console.log(d)
+
+console.log(Math.floor(a*10)+1)  // with random number from variable a
+
+
+
